053b673dc27d1b0d75606821d75d218bfeb4c6af
Carlos Carrasquillo <36614712+CarlosCarras@users.noreply.github.com>
2021-05-22 00:17:07 -0400

    added ability to select pwm channels prior to compilation

--- voltage2pwm.cpp
+++ voltage2pwm.cpp
@@ -12,6 +12,8 @@
 
 #include "PWM.h"
 
+int pwm_channels[NUM_PWM_CHANNELS] = {0, 1, 2};
+
 
 unsigned long voltage2pwm(float voltage) {
 	return (voltage - MIN_VOLTAGE) * (MAX_PWM_PERIOD - MIN_PWM_PERIOD) / (MAX_VOLTAGE - MIN_VOLTAGE) + MIN_PWM_PERIOD;	// mapping function
@@ -25,7 +27,7 @@ int updatePWM(float* voltage) {
 
 	for (int i = 0; i < NUM_PWM_CHANNELS; i++) {
 		period = voltage2pwm(voltage[i]);
-		status += set_period(i, period_ns);
+		status += set_period(pwm_channels[i], period_ns);
 	}
 
 	return status;
